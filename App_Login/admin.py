b01c7202cac7851eb1325bf00eb132d7fbb6f36d
Rafid41 <wap.alnahiyan425@gmail.com>
2025-08-03 02:42:32 +0600

    User group Beta

--- App_Login/admin.py
+++ App_Login/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import User_Group
 # Register your models here.
+admin.site.register(User_Group)
--- App_Login/models.py
+++ App_Login/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
+
+
+class User_Group(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ("View History and Status only", "View History and Status only"),
+        ("Editor", "Editor"), 
+    ]
+    user_group_type = models.CharField(
+        max_length=100, choices=STATUS_CHOICES, default="View History and Status only"
+    )
+
+
+    def __str__(self):
+        return str(self.user.username)
