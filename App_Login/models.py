221007183ff2f45da19f08d22c6f158f8c32124e
Rafid41 <wap.alnahiyan425@gmail.com>
2025-08-06 01:58:02 +0600

    some fix

--- App_Login/models.py
+++ App_Login/models.py
@@ -7,6 +7,8 @@ class User_Group(models.Model):
     STATUS_CHOICES = [
         ("View History and Status only", "View History and Status only"),
         ("Editor", "Editor"), 
+        ("Only View History and Edit CS&M Column", "Only View History and Edit CS&M Column"),
+        ("Only View History and Edit Carry_From_Warehouse Column", "Only View History and Edit Carry_From_Warehouse Column"),
     ]
     user_group_type = models.CharField(
         max_length=100, choices=STATUS_CHOICES, default="View History and Status only"
