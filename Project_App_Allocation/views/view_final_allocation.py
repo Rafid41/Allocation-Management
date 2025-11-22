e68399d24ca4e51d5a498d71fbc7b1e46b6dfd40
Rafid41 <wap.alnahiyan425@gmail.com>
2025-11-22 11:34:34 +0600

    Project_App_Allocation: view final allocation will now shows temprary Allocation lists

--- Project_App_Allocation/views/view_final_allocation.py
+++ Project_App_Allocation/views/view_final_allocation.py
@@ -1,25 +1,41 @@
 # App_Allocation\views\view_final_allocation.py
+
+#####################################################################
+### VVI: This now shows Temporay_Allocation, not Final_Allocation ###
+#####################################################################
+
+
 from django.shortcuts import render
-from Project_App_Allocation.models import Final_Allocation
+from Project_App_Allocation.models import Temporary_Allocation
+from django.db.models import Q
 
 def final_allocation_search(request):
     query = request.GET.get("query", "")
     filter_by = request.GET.get("filter_by", "All")
     date_filter = request.GET.get("date", "")
 
-    results = Final_Allocation.objects.all().order_by('-created_at')
+    results = Temporary_Allocation.objects.all().order_by('-created_at')
 
     if query:
+        if filter_by == "All":
+            # search across all relevant fields with exact (case-insensitive) match
+            results = results.filter(
+                Q(allocation_no__allocation_no__iexact=query)
+                | Q(pbs__name__iexact=query)
+                | Q(project__projectId__iexact=query)
+                | Q(item__name__iexact=query)
+                | Q(warehouse__iexact=query)
+            )
         if filter_by == "allocation_no":
-            results = results.filter(allocation_no__allocation_no__icontains=query)
+            results = results.filter(allocation_no__allocation_no__iexact=query)
         elif filter_by == "pbs":
-            results = results.filter(pbs__name__icontains=query)
+            results = results.filter(pbs__name__iexact=query)
         elif filter_by == "project":
-            results = results.filter(project__projectId__icontains=query)
+            results = results.filter(project__projectId__iexact=query)
         elif filter_by == "item":
-            results = results.filter(item__name__icontains=query)
+            results = results.filter(item__name__iexact=query)
         elif filter_by == "warehouse":
-            results = results.filter(warehouse__icontains=query)
+            results = results.filter(warehouse__iexact=query)
 
     if date_filter:
         results = results.filter(created_at__date=date_filter)
