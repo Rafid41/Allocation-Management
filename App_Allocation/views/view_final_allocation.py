630fc5650f1a729134728165839436c57bd500f3
Rafid41 <wap.alnahiyan425@gmail.com>
2025-11-22 11:24:52 +0600

    App_Allocation: view final allocation will now shows temprary Allocation lists

--- App_Allocation/views/view_final_allocation.py
+++ App_Allocation/views/view_final_allocation.py
@@ -1,25 +1,40 @@
 # App_Allocation\views\view_final_allocation.py
+
+#####################################################################
+### VVI: This now shows Temporay_Allocation, not Final_Allocation ###
+#####################################################################
+
 from django.shortcuts import render
-from App_Allocation.models import Final_Allocation
+from App_Allocation.models import Temporary_Allocation
+from django.db.models import Q
 
 def final_allocation_search(request):
     query = request.GET.get("query", "")
     filter_by = request.GET.get("filter_by", "All")
     date_filter = request.GET.get("date", "")
 
-    results = Final_Allocation.objects.all().order_by('-created_at')
+    results = Temporary_Allocation.objects.all().order_by('-created_at')
 
     if query:
-        if filter_by == "allocation_no":
-            results = results.filter(allocation_no__allocation_no__icontains=query)
+        if filter_by == "All":
+            # search across all relevant fields with exact (case-insensitive) match
+            results = results.filter(
+                Q(allocation_no__allocation_no__iexact=query)
+                | Q(pbs__name__iexact=query)
+                | Q(package__packageId__iexact=query)
+                | Q(item__name__iexact=query)
+                | Q(warehouse__iexact=query)
+            )
+        elif filter_by == "allocation_no":
+            results = results.filter(allocation_no__allocation_no__iexact=query)
         elif filter_by == "pbs":
-            results = results.filter(pbs__name__icontains=query)
+            results = results.filter(pbs__name__iexact=query)
         elif filter_by == "package":
-            results = results.filter(package__packageId__icontains=query)
+            results = results.filter(package__packageId__iexact=query)
         elif filter_by == "item":
-            results = results.filter(item__name__icontains=query)
+            results = results.filter(item__name__iexact=query)
         elif filter_by == "warehouse":
-            results = results.filter(warehouse__icontains=query)
+            results = results.filter(warehouse__iexact=query)
 
     if date_filter:
         results = results.filter(created_at__date=date_filter)
