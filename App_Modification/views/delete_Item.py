7adabc0e5fa9cca268ab8f91394e42ac98715929
Rafid41 <wap.alnahiyan425@gmail.com>
2026-01-11 13:04:59 +0600

    App_Modification: delete item dependent search

--- App_Modification/views/delete_Item.py
+++ App_Modification/views/delete_Item.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+import json
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
@@ -12,12 +13,42 @@ def view_final_allocation(request, allocation_id):
     allocation_no_obj = get_object_or_404(Allocation_Number, id=allocation_id)
     allocations = Final_Allocation.objects.filter(allocation_no=allocation_no_obj)
 
+    # Filter logic
+    applied_filters = []
+    for i in range(1, 10):
+        filter_type = request.GET.get(f"filter_type_{i}")
+        filter_value = request.GET.get(f"filter_value_{i}")
+
+        if filter_value:
+            filter_value = filter_value.strip()
+
+        if not filter_type or filter_type == "no_condition" or not filter_value:
+            continue
+        
+        applied_filters.append({
+            "type": filter_type,
+            "value": filter_value
+        })
+
+        if filter_type == "pbs":
+            allocations = allocations.filter(pbs__name__iexact=filter_value)
+        elif filter_type == "package":
+            allocations = allocations.filter(package__packageId__iexact=filter_value)
+        elif filter_type == "item":
+            allocations = allocations.filter(item__name__iexact=filter_value)
+        elif filter_type == "warehouse":
+            allocations = allocations.filter(warehouse__iexact=filter_value)
+
+    # Get distinct warehouses for dropdown
+    warehouses = list(Final_Allocation.objects.values_list('warehouse', flat=True).distinct())
+    
     return render(
         request,
         "App_Modification/view_and_delete_Item.html",
         {
             "allocations": allocations,
             "allocation_no_obj": allocation_no_obj,
+            "warehouse_json": json.dumps(warehouses),
         },
     )
 
