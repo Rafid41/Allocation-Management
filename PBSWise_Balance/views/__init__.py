36debaec010e31c380c55566ee74800263db7cde
Rafid41 <wap.alnahiyan425@gmail.com>
2026-03-19 02:16:29 +0600

    PBSWise Balance app

---
+++ PBSWise_Balance/views/__init__.py
@@ -0,0 +1,1 @@
+from .all_pbs_list_page import *
--- PBSWise_Home/views.py
+++ PBSWise_Home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def home_page(request):
     """View to render the PBS-Wise Home Page."""
-    return render(request, "PBSWie_Templates/PBSWise_Home/PBSWise_home_page.html")
+    return render(request, "PBSWise_Templates/PBSWise_Home/PBSWise_home_page.html")
