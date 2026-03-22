f7633e43ddaf0e930bbcc502b80d7851f28b9bc3
Rafid41 <wap.alnahiyan425@gmail.com>
2026-03-22 15:04:41 +0600

    StoreWise balance init app

---
+++ PBSWise_StoreWise_Balance/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PbswiseStorewiseBalanceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PBSWise_StoreWise_Balance'
